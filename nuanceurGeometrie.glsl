288842db621d731349e6b94255fa39e1d69a388e
sergepancho <43280624+sergepancho@users.noreply.github.com>
2019-11-07 15:28:58 -0500

    ajout du tp3

---
+++ nuanceurGeometrie.glsl
@@ -0,0 +1,43 @@
+#version 430
+layout(triangles) in;layout(triangle_strip, max_vertices = 6) out;
+
+
+in Attribs {
+    vec4 couleur;
+	float sortie1,sortie2;
+
+} AttribsIn[];
+
+
+
+out Attribs {
+    vec4 couleur;
+
+} AttribsOut;
+
+
+void main()
+{
+    // émettre les sommets
+    for ( int i = 0 ; i < gl_in.length() ; ++i )
+    {
+        gl_ViewportIndex = 0;
+        gl_Position = gl_in[i].gl_Position;
+        AttribsOut.couleur= AttribsIn[i].couleur;
+        gl_ClipDistance[0] = AttribsIn[i].sortie1;
+		gl_ClipDistance[1] = AttribsIn[i].sortie2;
+        EmitVertex();
+    }
+    EndPrimitive();
+
+    for ( int i = 0 ; i < gl_in.length() ; ++i )
+    {
+        gl_ViewportIndex = 1;
+        gl_Position = gl_in[i].gl_Position;
+		AttribsOut.couleur= vec4(1- AttribsIn[i].couleur[0],1- AttribsIn[i].couleur[1],1- AttribsIn[i].couleur[2],AttribsIn[i].couleur[0]);
+        gl_ClipDistance[0] = AttribsIn[i].sortie1;
+		gl_ClipDistance[1] = AttribsIn[i].sortie2;
+        EmitVertex();
+    }
+    EndPrimitive();
+}
